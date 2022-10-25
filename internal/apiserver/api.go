e0df037350c6226a5bba88f64e0c31d3f789d646
Rajashekar Reddy Kommula <32640055+Rajashekarredde@users.noreply.github.com>
2022-10-24 19:40:24 -0700

    Update api.go

--- internal/apiserver/api.go
+++ internal/apiserver/api.go
@@ -1,47 +1,21 @@
 package apiserver
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
-
-	"github.com/go-chi/render"
-	"grandChallenge1/grpcchat123/conf"
-	"grandChallenge1/grpcchat123/internal/chatserver/client"
-	"grandChallenge1/grpcchat123/internal/chatserver/client1"
-)
-
-type Api struct {
-	Cnf *conf.Conf
-}
-
-func (api *Api) GetMessage(w http.ResponseWriter, r *http.Request) {
+import ( "github.com/go-chi/render" "grandChallenge1/grpcchat123/conf" "grandChallenge1/grpcchat123/internal/chatserver/client"
+	"grandChallenge1/grpcchat123/internal/chatserver/client1" "encoding/json" "fmt" "io/ioutil" "net/http" "strings"
+        )
+type Api struct { Cnf *conf.Conf }
+func (api *Api) GetMessage(w http.ResponseWriter, r *http.Request) 
+{
 	data, err := ioutil.ReadFile(api.Cnf.ChatLog)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	}
+	if err != nil 
+	{ w.WriteHeader(http.StatusInternalServerError) w.Write([]byte("Internal server error")) return }
 	line_arr := strings.Split(string(data), "\n")
 	render.JSON(w, r, line_arr)
 }
-
-func (api *Api) PostMessage(w http.ResponseWriter, r *http.Request) {
-	name := "Satvik"
-	room_name := "default"
-	blocked_name := "NA"
-	params := &PostParams{}
-	err := json.NewDecoder(r.Body).Decode(&params)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-	if params.Name == "" {
-		params.Name = name
-	}
+func (api *Api) PostMessage(w http.ResponseWriter, r *http.Request) { name := "Satvik" room_name := "default" blocked_name := "NA"
+	params := &PostParams{} err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil { w.WriteHeader(http.StatusInternalServerError) w.Write([]byte("Internal server error")) return }
+	if params.Name == "" {	params.Name = name }
 	if params.RoomName == "" {
 		params.RoomName = room_name
 	}
@@ -51,7 +25,6 @@ func (api *Api) PostMessage(w http.ResponseWriter, r *http.Request) {
 		BlockedName: blocked_name,
 	}
 	fmt.Println(client.Name, client.RoomName)
-	//err = client.ExecApi(params.Message)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Internal server error"))
